2db15d30b02c697e8a21d7edad340a414692c8bb
RamiruWanigathunga <studentworkings123@gmail.com>
2026-03-17 15:33:41 +0530

    v2.4

--- s1.py
+++ s1.py
@@ -1,15 +1,53 @@
 #Question 1
+print("Ayesha Wanigathunga")
+print("National School of Business Management Green University")
 
-print("Hello, World!")
-
-# single line commenbt
+# single line comment
+# This is a single line comment
 
 """
     This is a multi-line comment
+    We use comments in Python to explain what our code is doing, 
+    to make it easier for others to understand what we are trying to do, 
+    and to debug our code more easily.
 """
 
 #Question 2
 a = 22
 b = 7
+print("Result of 22 divided by 7 is " + str(a/b))
 
-print(a/b)
+#Question 3
+operation = "sum"
+total = 8
+print("The " + operation + " of two numbers is " + str(total))
+
+#Question 4
+length = 10
+width = 5
+print("The area of the rectangle is " + str(length*width) + " square units")
+
+#Question 5
+name = input("Please enter your name: ")
+print("Welcome " + name + "!")
+print("The data type of the variable name is " + str(type(name)))
+
+#Question 6
+birth_year = int(input("Please enter your birth year: "))
+print("Your birth year is: " + str(birth_year))
+print("The data type of the variable birth_year is " + str(type(birth_year)))
+
+#Question 7
+birth_year = int(input("Please enter your birth year: "))
+age = 2025 - birth_year
+print("Your age is: " + str(age))
+
+#Bonus Question
+num1 = float(input("Please enter the first number: "))
+num2 = float(input("Please enter the second number: "))
+print("The sum of the two numbers is: " + str(num1 + num2))
+print("The difference of the two numbers is: " + str(num1 - num2))
+print("The product of the two numbers is: " + str(num1 * num2))
+print("The division of the two numbers is: " + str(num1 / num2))
+print("The power of the two numbers is: " + str(num1 ** num2))
+
