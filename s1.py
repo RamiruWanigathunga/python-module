33b0e4140607cb8ab17a923b89929432af7849c9
RamiruWanigathunga <studentworkings123@gmail.com>
2026-03-17 15:44:34 +0530

    v2.8

--- s1.py
+++ s1.py
@@ -19,12 +19,12 @@ print("Result of 22 divided by 7 is " + str(a/b))
 
 #Question 3
 operation = "sum"
-total = 8
+total = 9993
 print("The " + operation + " of two numbers is " + str(total))
 
 #Question 4
-length = 10
-width = 5
+length = 20
+width = 7
 print("The area of the rectangle is " + str(length*width) + " square units")
 
 #Question 5
@@ -39,15 +39,15 @@ print("The data type of the variable birth_year is " + str(type(birth_year)))
 
 #Question 7
 birth_year = int(input("Please enter your birth year: "))
-age = 2025 - birth_year
+age = 2026 - birth_year
 print("Your age is: " + str(age))
 
-#Bonus Question
-num1 = float(input("Please enter the first number: "))
-num2 = float(input("Please enter the second number: "))
-print("The sum of the two numbers is: " + str(num1 + num2))
-print("The difference of the two numbers is: " + str(num1 - num2))
-print("The product of the two numbers is: " + str(num1 * num2))
-print("The division of the two numbers is: " + str(num1 / num2))
-print("The power of the two numbers is: " + str(num1 ** num2))
+#The Bonus Question
+number_1 = float(input("Please enter the first number: "))
+number_2 = float(input("Please enter the second number: "))
+print("The sum of the two numbers is: " + str(number_1 + number_2))
+print("The difference of the two numbers is: " + str(number_1 - number_2))
+print("The product of the two numbers is: " + str(number_1 * number_2))
+print("The division of the two numbers is: " + str(number_1 / number_2))
+print("The power of the two numbers is: " + str(number_1 ** number_2))
 
