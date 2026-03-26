203a00d8c4de31f8c914bac6cd38ae8e24dbfa08
RamiruWanigathunga <studentworkings123@gmail.com>
2026-03-26 09:28:49 +0530

    v3.1

---
+++ s3.py
@@ -0,0 +1,11 @@
+a=float(input("Enter the first number: "))
+b=float(input("Enter the second number: "))
+
+if a>b:
+    print("The first number is greater than the second number.")
+elif a<b:
+    print("The second number is greater than the first number.")
+elif a==b:
+    print("Both numbers are equal.")
+else:    
+    print("Invalid input.")
