337495cbfd748afeb2195a59bf19b61e6144f96c
RamiruWanigathunga <studentworkings123@gmail.com>
2026-03-26 09:37:33 +0530

    v3.3

--- s3.py
+++ s3.py
@@ -1,3 +1,6 @@
+#Question 1
+#Write a Python program to find the largest of two numbers entered by the user.
+'''
 a=float(input("Enter the first number: "))
 b=float(input("Enter the second number: "))
 
@@ -9,3 +12,16 @@ elif a==b:
     print("Both numbers are equal.")
 else:    
     print("Invalid input.")
+'''
+#Question 2
+#Write a Python program to check a person is eligible to vote or not.
+
+age=input("Enter your age: ")
+if age.isdigit():
+    age=int(age)
+    if age>=18:
+        print("You are eligible to vote.")
+    else:
+        print("You are not eligible to vote.")
+else:
+    print("Invalid input. Please enter a valid age.")
